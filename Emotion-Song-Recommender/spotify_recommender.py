c5a4f2c4504995cf7cae0af9b77e29ba36c14787
skashyap08 <sakshikashyap9876@gmail.com>
2026-06-11 12:15:37 +0530

    song rec code updated

--- Emotion-Song-Recommender/spotify_recommender.py
+++ Emotion-Song-Recommender/spotify_recommender.py
@@ -1,12 +1,45 @@
-import pandas as pd
-import random
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-def recommend_song(emotion):
-    df = pd.read_csv("songs.csv")
+CLIENT_ID = "YOUR_CLIENT_ID"
+CLIENT_SECRET = "YOUR_CLIENT_SECRET"
 
-    songs = df[df['emotion'] == emotion]['song'].tolist()
+sp = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET
+    )
+)
 
-    if songs:
-        return random.choice(songs)
-    else:
-        return "No song found"
+emotion_queries = {
+    "happy": "happy playlist",
+    "sad": "sad songs",
+    "angry": "rock workout",
+    "fear": "calm relaxing music",
+    "neutral": "chill hits",
+    "surprise": "party songs",
+    "disgust": "motivational songs"
+}
+
+def recommend_playlist(emotion):
+
+    query = emotion_queries.get(
+        emotion,
+        "top hits"
+    )
+
+    results = sp.search(
+        q=query,
+        type="playlist",
+        limit=5
+    )
+
+    playlists = []
+
+    for item in results["playlists"]["items"]:
+        playlists.append({
+            "name": item["name"],
+            "url": item["external_urls"]["spotify"]
+        })
+
+    return playlists
